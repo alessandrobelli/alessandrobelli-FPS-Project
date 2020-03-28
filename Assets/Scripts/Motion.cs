976cd4b07f8f36d361ead715463dec7575517505
Alessandro Belli <alessandrobelli90@gmail.com>
2020-03-28 20:47:29 +0100

    First commit

---
+++ Assets/Scripts/Motion.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Com.Nudi.Fpsproject
+{
+
+    public class Motion : MonoBehaviour
+    {
+
+        public float speed;
+
+        private Rigidbody rig;
+
+        public void Start()
+        {
+            Camera.main.enabled = false;
+            rig = GetComponent<Rigidbody>();
+        }
+
+        // Update is called once per frame
+        void FixedUpdate()
+        {
+            float t_hmove = Input.GetAxis("Horizontal");
+            float t_vmove = Input.GetAxis("Vertical");
+
+            Vector2 t_direction = new Vector3(t_hmove, 0, t_vmove);
+            t_direction.Normalize();
+
+            rig.velocity = transform.TransformDirection(t_direction) * speed * Time.fixedDeltaTime;
+
+
+
+        }
+    }
+}
