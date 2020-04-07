a85eb4557a77f0ae529404a43d30c66b243c913a
Alessandro Belli <alessandrobelli90@gmail.com>
2020-04-07 20:05:51 +0200

    Photon + Start Menu
    * imported Photon PUN for networking.
    * started creating the menu with networking features.

---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+namespace Com.Nudi.Fpsproject
+{
+    public class Menu : MonoBehaviour
+    {
+        private void OnEnable() 
+        {
+            PhotonNetwork.AutomaticallySyncScene = true;
+            Connect();
+        }
+
+        public void Connect()
+        {
+
+        }
+
+        public void Join()
+        {
+
+        }
+
+        public void StartGame()
+        {
+
+        }
+    }
+}
