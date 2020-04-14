3290d510096534212ce05dfb284a8b20c8247812
Alessandro Belli <alessandrobelli90@gmail.com>
2020-04-14 18:05:20 +0200

    Sliding
    * now the player can slide.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -13,6 +13,8 @@ namespace Com.Nudi.Fpsproject
 
         #region Variables
         public float speed;
+        public float slideModifier;
+        public float lengthOfSlide;
         public float sprintModifier;
         public float jumpForce;
         public int max_health;
@@ -20,21 +22,26 @@ namespace Com.Nudi.Fpsproject
         public GameObject cameraParent;
         public Transform weaponParent;
         public Transform groundDetector;
-        private Vector3 targetWeaponBobPosition;
         public LayerMask ground;
+ 
+
         private Rigidbody rig;
         private float movementCounter;
         private float idleCounter;
         private Vector3 weaponParentOrigin;
         private float baseFOV;
         private float sprintFOVModifier = 1.5f;
-
         private int current_health;
         private Manager manager;
         private Weapon weapon;
         private Transform ui_healthbar;
         private UnityEngine.UI.Text ui_ammo;
-
+        private Vector3 targetWeaponBobPosition;
+        private bool sliding;
+        private float slide_time;
+        private Vector3 slide_dir;
+        private Vector3 normalCameraOrigin;
+        private Vector3 weaponParentCurrentPosition;
 
         #endregion
 
@@ -51,10 +58,12 @@ namespace Com.Nudi.Fpsproject
 
 
             baseFOV = normalCam.fieldOfView;
+            normalCameraOrigin = normalCam.transform.localPosition;
 
             if (Camera.main) Camera.main.enabled = false;
             rig = GetComponent<Rigidbody>();
             weaponParentOrigin = weaponParent.localPosition;
+            weaponParentCurrentPosition = weaponParentOrigin;
 
             if (photonView.IsMine)
             {
@@ -93,23 +102,26 @@ namespace Com.Nudi.Fpsproject
             }
 
             // headbob
-            if (t_hmove == 0 && t_vmove == 0)
+            if (!sliding) 
             {
-                HeadBob(idleCounter, 0.025f, 0.025f);
-                idleCounter += Time.deltaTime;
-                weaponParent.localPosition = Vector3.Lerp(weaponParent.localPosition, targetWeaponBobPosition, Time.deltaTime * 2f);
-            }
-            else if (!isSprinting)
-            {
-                HeadBob(movementCounter, 0.035f, 0.035f);
-                movementCounter += Time.deltaTime * 3f;
-                weaponParent.localPosition = Vector3.Lerp(weaponParent.localPosition, targetWeaponBobPosition, Time.deltaTime * 6f);
-            }
-            else
-            {
-                HeadBob(movementCounter, 0.12f, 0.060f);
-                movementCounter += Time.deltaTime * 7f;
-                weaponParent.localPosition = Vector3.Lerp(weaponParent.localPosition, targetWeaponBobPosition, Time.deltaTime * 10f);
+                if (t_hmove == 0 && t_vmove == 0)
+                {
+                    HeadBob(idleCounter, 0.025f, 0.025f);
+                    idleCounter += Time.deltaTime;
+                    weaponParent.localPosition = Vector3.Lerp(weaponParent.localPosition, targetWeaponBobPosition, Time.deltaTime * 2f);
+                }
+                else if (!isSprinting)
+                {
+                    HeadBob(movementCounter, 0.035f, 0.035f);
+                    movementCounter += Time.deltaTime * 3f;
+                    weaponParent.localPosition = Vector3.Lerp(weaponParent.localPosition, targetWeaponBobPosition, Time.deltaTime * 6f);
+                }
+                else
+                {
+                    HeadBob(movementCounter, 0.12f, 0.060f);
+                    movementCounter += Time.deltaTime * 7f;
+                    weaponParent.localPosition = Vector3.Lerp(weaponParent.localPosition, targetWeaponBobPosition, Time.deltaTime * 10f);
+                }
             }
 
             // UI Refreshes
@@ -128,37 +140,81 @@ namespace Com.Nudi.Fpsproject
 
             // control
             float t_adjustedSpeed = speed;
-            bool jump = Input.GetKeyDown(KeyCode.Space);
-            bool sprint = Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift);
+            bool jumpButtonPressed = Input.GetKeyDown(KeyCode.Space);
+            bool sprintButtonPressed = Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift);
             bool notMovingBackwards = t_vmove > 0;
+            bool slideButtonPressed = Input.GetKey(KeyCode.C);
 
-            // states
+            // #states
             bool isGrounded = Physics.Raycast(groundDetector.position, Vector3.down, 0.1f, ground);
-            bool isJumping = jump && isGrounded;
-            bool isSprinting = sprint && notMovingBackwards && !isJumping && isGrounded;
+            bool isJumping = jumpButtonPressed && isGrounded;
+            bool isSprinting = sprintButtonPressed && notMovingBackwards && !isJumping && isGrounded;
+            bool isSliding = isSprinting && slideButtonPressed && !sliding;
+
 
             // movement
-            Vector3 t_direction = new Vector3(t_hmove, 0, t_vmove);
-            t_direction.Normalize();
-
-
-
-
-            // FOV
-            if (isSprinting)
+            Vector3 t_direction = Vector3.zero;
+            if (!sliding)
             {
+                t_direction = new Vector3(t_hmove, 0, t_vmove);
+                t_direction.Normalize();
+
                 t_adjustedSpeed *= sprintModifier;
-                normalCam.fieldOfView = Mathf.Lerp(normalCam.fieldOfView, baseFOV * sprintFOVModifier, Time.deltaTime * 8f);
+                t_direction = transform.TransformDirection(t_direction);
+
             }
             else
             {
-                normalCam.fieldOfView = Mathf.Lerp(normalCam.fieldOfView, baseFOV, Time.deltaTime * 8f);
+                 t_direction = slide_dir;
+                t_adjustedSpeed *= slideModifier;
 
+                slide_time -= Time.fixedDeltaTime;
+                if (slide_time <= 0)
+                {
+                    sliding = false;
+                    weaponParentCurrentPosition += Vector3.up * 0.5f;
+                }
             }
 
-            Vector3 t_targetVelocity = transform.TransformDirection(t_direction) * t_adjustedSpeed * Time.fixedDeltaTime;
+
+
+            Vector3 t_targetVelocity = t_direction * t_adjustedSpeed * Time.fixedDeltaTime;
             t_targetVelocity.y = rig.velocity.y;
             rig.velocity = t_targetVelocity;
+
+            // sliding
+            if (isSliding)
+            {
+                sliding = true;
+                slide_dir = t_direction;
+                slide_time = lengthOfSlide;
+                weaponParentCurrentPosition += Vector3.down * 0.5f;
+                // adjust camera
+            }
+
+            // Camera Stuff
+            if (sliding)
+            {
+                normalCam.fieldOfView = Mathf.Lerp(normalCam.fieldOfView, baseFOV * sprintFOVModifier * 1.25f, Time.deltaTime * 8f);
+                normalCam.transform.localPosition = Vector3.Lerp(normalCam.transform.localPosition, normalCameraOrigin + Vector3.down * 0.5f, Time.deltaTime * 6f);
+            }
+            else
+            {
+                if (isSprinting)
+                {
+
+                    normalCam.fieldOfView = Mathf.Lerp(normalCam.fieldOfView, baseFOV * sprintFOVModifier, Time.deltaTime * 8f);
+                }
+                else
+                {
+                    normalCam.fieldOfView = Mathf.Lerp(normalCam.fieldOfView, baseFOV, Time.deltaTime * 8f);
+
+                }
+
+                normalCam.transform.localPosition = Vector3.Lerp(normalCam.transform.localPosition, normalCameraOrigin, Time.deltaTime * 6f);
+            }
+
+
 
 
         }
@@ -168,7 +224,7 @@ namespace Com.Nudi.Fpsproject
 
         void HeadBob(float z, float x_intensity, float y_intensity)
         {
-            if(!Input.GetMouseButton(1)) targetWeaponBobPosition = weaponParentOrigin + new Vector3(Mathf.Cos(z) * x_intensity, Mathf.Sin(z * 2) * y_intensity, weaponParentOrigin.z);
+            if (!Input.GetMouseButton(1)) targetWeaponBobPosition = weaponParentCurrentPosition + new Vector3(Mathf.Cos(z) * x_intensity, Mathf.Sin(z * 2) * y_intensity, weaponParentOrigin.z);
         }
 
         private void RefreshHealthBar()
