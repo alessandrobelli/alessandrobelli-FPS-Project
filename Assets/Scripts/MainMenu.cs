a99a8965494a24606ea56474478c4cb92ea359d6
Alessandro Belli <alessandrobelli90@gmail.com>
2020-04-16 20:20:57 +0200

    Main Menu
    * Main Menu.
    * Font Added.

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//using UnityEngine.SceneManagement;
+
+namespace Com.Nudi.Fpsproject
+{
+    public class MainMenu : MonoBehaviour
+    {
+        public MultiplayerController MultiplayerController;
+
+        public void JoinMatch()
+        {
+            MultiplayerController.Join();
+            // if not multiplayer
+            // unity engine scene management + scenemanager loadscene
+        }
+
+        public void CreateMatch()
+        {
+            MultiplayerController.Create();
+        }
+
+        public void QuitGame()
+        {
+            Application.Quit();
+        }
+
+
+    }
+}
---
+++ Assets/Scripts/MultiplayerController.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+namespace Com.Nudi.Fpsproject
+{
+    public class MultiplayerController : MonoBehaviourPunCallbacks
+    {
+        public void Awake()
+        {
+            PhotonNetwork.AutomaticallySyncScene = true;
+            Connect();
+        }
+
+        // once connected to photon
+        public override void OnConnectedToMaster()
+        {
+            base.OnConnectedToMaster();
+        }
+
+        public void Connect()
+        {
+            PhotonNetwork.GameVersion = "0.0.0";
+            PhotonNetwork.ConnectUsingSettings();
+
+        }
+
+        public void Join()
+        {
+            PhotonNetwork.JoinRandomRoom();
+
+        }
+
+        public override void OnJoinedRoom()
+        {
+            StartGame();
+
+            base.OnJoinedRoom();
+
+        }
+
+        public override void OnJoinRandomFailed(short returnCode, string message)
+        {
+            Create();
+
+            base.OnJoinRandomFailed(returnCode, message);
+
+        }
+
+        public void Create()
+        {
+            PhotonNetwork.CreateRoom("");
+        }
+
+        public void StartGame()
+        {
+            if (PhotonNetwork.CurrentRoom.PlayerCount == 1)
+            {
+                PhotonNetwork.LoadLevel(1);
+            }
+
+        }
+    }
+}
